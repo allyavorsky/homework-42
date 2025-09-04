ef6cc646e6cb05e4f16b2d115e628a426f95aee2
allyavorsky <allyavorsky@proton.me>
2025-09-04 10:28:21 +0300

    HW42-5: implement NavLink for active styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
@@ -8,9 +8,9 @@ function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Головна</Link>
-        <Link to="/about">Про нас</Link>
-        <Link to="/contact">Контакти</Link>
+        <NavLink to="/">Головна</NavLink>
+        <NavLink to="/about">Про нас</NavLink>
+        <NavLink to="/contact">Контакти</NavLink>
       </nav>
 
       <hr />
